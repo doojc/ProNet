d230a114d5853370a18eeed6c700a5726dea2a29
DoojC <dujso.edin@gmail.com>
2015-05-07 17:05:49 +0200

    Observable Collection

---
+++ FunWithGenericCollections/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithGenericCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UseGenericList();
+
+            UseGenericStack();
+
+            UseGenericQueue();
+
+            UseSortedSet();
+
+            Console.ReadLine();
+        }
+
+        static void UseGenericList()
+        {
+            // Make a List of Person objects, filled with collection/object init syntax.
+            List<Person> people = new List<Person>()
+            {
+                new Person { FirstName = "Homer", LastName = "Simpson", Age = 47},
+                new Person { FirstName = "Marge", LastName = "Simpson", Age = 45},
+                new Person { FirstName = "Lisa", LastName = "Simpson", Age = 9 }
+            };
+
+            // Print out # of items in list
+            Console.WriteLine("Items in the list: {0}", people.Count);
+
+            // Enumerate over list.
+            foreach (Person p in people)
+            {
+                Console.WriteLine(p);
+            }
+
+            // Insert new person.
+            Console.WriteLine("\n->Inserting new person.");
+            people.Insert(2, new Person { FirstName = "Bart", LastName = "Simpson", Age = 8 });
+            Console.WriteLine("Items in list: {0}", people.Count);
+
+            // Copy data into a new array.
+            Person[] arrayOfPeople = people.ToArray();
+            for (int i = 0; i < arrayOfPeople.Length; i++)
+            {
+                Console.WriteLine("First Names: {0}", arrayOfPeople[i].FirstName);
+            }
+
+        }
+
+        static void UseGenericStack()
+        {
+            Stack<Person> stackOfPeople = new Stack<Person>();
+            stackOfPeople.Push(new Person { FirstName = "Homer", LastName = "Simpson", Age = 49 });
+            stackOfPeople.Push(new Person { FirstName = "Marge", LastName = "Simpson", Age = 47 });
+            stackOfPeople.Push(new Person { FirstName = "Lisa", LastName = "Simpson", Age = 9 });
+
+            // Now look at the top item, pop it, and look again.
+            Console.WriteLine("First person is: {0}", stackOfPeople.Peek());
+            Console.WriteLine("Popped off {0}", stackOfPeople.Pop());
+
+            Console.WriteLine("\nFirst person is: {0}", stackOfPeople.Peek());
+            Console.WriteLine("Popped off {0}", stackOfPeople.Pop());
+
+            Console.WriteLine("\nFirst person is: {0}", stackOfPeople.Peek());
+            Console.WriteLine("Popped off {0}", stackOfPeople.Pop());
+
+            try
+            {
+                Console.WriteLine("\nFirst person is: {0}", stackOfPeople.Peek());
+                Console.WriteLine("Popped off {0}", stackOfPeople.Pop());
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine("\nError! {0}", ex.Message);
+                
+            }
+        }
+    
+        static void GetCoffee(Person p)
+        {
+            Console.WriteLine("{0} got coffee!", p.FirstName);
+        }
+
+        static void UseGenericQueue()
+        {
+            // Make a Q with three people.
+            Queue<Person> peopleQ = new Queue<Person>();
+            peopleQ.Enqueue(new Person { FirstName = "Homer", LastName = "Simpson", Age = 47 });
+            peopleQ.Enqueue(new Person { FirstName = "Marge", LastName = "Simpson", Age = 45 });
+            peopleQ.Enqueue(new Person { FirstName = "Lisa", LastName = "Simpson", Age = 9 });
+
+            // Peek at first person in Q.
+            Console.WriteLine("{0} is first in line!", peopleQ.Peek().FirstName);
+
+            // Remove each person from Q.
+            GetCoffee(peopleQ.Dequeue());
+            GetCoffee(peopleQ.Dequeue());
+            GetCoffee(peopleQ.Dequeue());
+            
+            // Try to de-Q again.
+            try
+            {
+                GetCoffee(peopleQ.Dequeue());
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Error! {0}", e.Message);
+            }
+        }
+
+        static void UseSortedSet()
+        {
+            // Make some people with different ages.
+            SortedSet<Person> setOfPeople = new SortedSet<Person>(new SortPeopleByAge())
+            {
+                new Person { FirstName = "Homer", LastName = "Simpson", Age = 47 },
+                new Person { FirstName = "Marge", LastName = "Simpson", Age = 45 },
+                new Person { FirstName = "Lisa", LastName = "Simpson", Age = 9 },
+                new Person { FirstName = "Bart", LastName = "Simpson", Age = 8 }
+            };
+
+            foreach (Person p in setOfPeople)
+            {
+                Console.WriteLine(p);
+            }
+
+
+
+        }
+    
+    }
+}
---
+++ IssuesWithNonGenericCollections/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace IssuesWithNonGenericCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void SimpleBoxUnboxOperation()
+        {
+            // Make a ValueType (int) variable.
+            int myInt = 25;
+
+            // Box the int into an object reference.
+            object boxedInt = myInt;
+
+            // Unbox teh reference back into a corresponding int.
+            int unboxedInt = (int)boxedInt;
+        }
+    }
+}
