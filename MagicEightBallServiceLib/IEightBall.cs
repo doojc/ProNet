4383d35e08c3a62f36bf9a9ef229d71cfff469fc
DoojC <dujso.edin@gmail.com>
2015-07-22 16:02:58 +0200

    Magic8Ball and BasicMath WCF

---
+++ MagicEightBallServiceLib/IEightBall.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace MagicEightBallServiceLib
+{
+    [ServiceContract]
+    public interface IEightBall
+    {
+        // Ask a question, receive an answer
+        [OperationContract]
+        string ObtainAnswerToQuestion(string userQuestion);
+    }
+}
---
+++ MagicEightBallServiceLib/MagicEightBallService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace MagicEightBallServiceLib
+{
+    public class MagicEightBallService : IEightBall
+    {
+        // Just for display purposes on the host
+        public MagicEightBallService()
+        {
+            Console.WriteLine("The 8-ball awaits your question...");
+        }
+
+        public string ObtainAnswerToQuestion(string userQuestion)
+        {
+            string[] answers = { "Future Uncertain", "Yes", "No", "Hazy", "Ask again later", "Definitely" };
+
+            // Return random response
+            Random r = new Random();
+            return answers[r.Next(answers.Length)];
+        }
+    }
+}
